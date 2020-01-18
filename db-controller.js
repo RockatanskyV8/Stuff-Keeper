1ce24d1d59d7454f86b8847985d35242185932d3
RockatanskyV8 <luccasanttoz@gmail.com>
2020-01-18 14:40:15 -0300

    alterando fomra de leitura da mensagem

--- db-controller.js
+++ db-controller.js
@@ -1,22 +1,17 @@
 'use strict'
 
 let paths = {
-  'users' : {
-              'get'      : "select name from msgs.users;",
-              'specific' : "select * from msgs.users where name = $1;",
-              'create'   : "insert into msgs.users (name) values ($1);",
-              'update'   : "update msgs.users set name = $2 where id = $1;",
-              'delete'   : "delete from msgs.users where id = $1;"
-            },
-  'posts' : {
-              'get'      : "select * from msgs.posts;",
-              'specific' : "select * from msgs.posts where id = $1;",
-              'create'   : "insert into msgs.posts (id_usr, post) values ($1, $2);",
-              'update'   : "update msgs.posts set post = $3 where id = $1 and id_usr = $2;",
-              'delete'   : "delete from msgs.posts where id = $1;"
-            }
+    'get'      : "select name from msgs.users limit 1;",
+    'specific' : "select * from msgs.users where name = $1;",
+    'create'   : "insert into msgs.users (name) values ($1);",
+    'update'   : "update msgs.users set name = $2 where id = $1;",
+    'delete'   : "delete from msgs.users where id = $1;"
 }
 
-exports.rule = (param) => {
-  return (paths[ param[0] ][ param[1] ])
+exports.rule = (args) => {
+  let commands = args.split("-")
+  commands[1] = (commands[1].trim()).split(",")
+
+  return [paths[ commands[0] ] , commands[1]]
 }
+// bot.sendMessage(chatID, convertida.toString().split(",").join(""));
--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 
 const TelegramBot = require( `node-telegram-bot-api` )
+
 const controller  = require( './db-controller' )
 const { Client } = require('pg');
 const connectionString = 'postgres://postgres:@localhost:5432/mssg';
-
 const client = new Client({ connectionString: connectionString });
 client.connect();
 
+let paths = {
+    'get'      : "select id || ' - ' || name as nfo from msgs.users order by id;",
+    'specific' : "select * from msgs.users where name = $1;",
+    'create'   : "insert into msgs.users (name) values ($1);",
+    'update'   : "update msgs.users set name = $2 where id = $1;",
+    'delete'   : "delete from msgs.users where id = $1;"
+}
+
 const bot = new TelegramBot( '1034299915:AAEmklqTtBXpoME_bI57Fb34fkQnMlLPXuo', { polling: true } )
 
-bot.on('message', (msg) => {
-  const chatID = msg.chat.id;
-  const a = (msg.text).split(' - ')
-
-  client.query(controller.rule(a), function (err, result) {
-    if (err) { console.log(err); }
-    let convertida = (result.rows).map(function(obj) {
-      return Object.keys(obj).map(function(chave) {
-          return obj[chave] + "\n";
-      })[0];
-    });
-    bot.sendMessage(chatID, convertida.toString().split(",").join(""));
+bot.onText(/Get/, (msg) => {
+  let chatID = msg.chat.id;
+  console.log(chatID)
+  let query  = paths[(msg.text).toLowerCase()];
+  client.query(query, (err, res) => { let x;
+                                      for(x in res['rows']){ bot.sendMessage(chatID, res['rows'][x]['nfo']) }
   });
+});
+
+bot.onText(/P [0-9]+l \- [0-9\,]+ \- [0-9]+/, (msg) => {
+  let chatID = msg.chat.id;
+  let regex = '/([0-9]+l) \- ([0-9\,]+) \- ([0-9]+)/g'.exec(msg.text)
+  // let text = (msg.text).replace("P ", "").split(" - ");
+
+
+  console.log(regex)
 });
